88cdbeb8117698e8ed94c09a4487e86935ed6289
emalang <109810872+emalang@users.noreply.github.com>
2026-04-09 10:23:16 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express'); // Uvoz Express frameworka
+const path = require('path'); // Uvoz modula path za rad s putanjama datoteka
+
+const app = express(); // Inicijalizacija Express aplikacije
+const PORT = process.env.PORT || 3000; // Postavljanje porta na kojem će server slušati
+
+// Poslužuje sve statičke datoteke iz mape public
+app.use(express.static(path.join(__dirname, 'Web-programming_LV1', 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'Web-programming_LV1', 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server je pokrenut na http://localhost:${PORT}`);
+});
